67f97de6c11182780459c97ed0e065c7df245a1d
A Bourne <andrewmbourne@gmail.com>
2020-08-12 13:47:39 +0100

    bulk inserts, and retry around web request

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 'use strict';
 
 const path = require('path');
-const database = require('sqlite-async');
+const database = require('better-sqlite3');
 const program = require('commander');
-const moment = require('moment');
+const retry = require('async-retry-ng');
 const axios = require('axios');
 
-const main = async() => {
-  try {
-    const filename = moment().format('Y-M-D H:m:s');
-    const db_name = path.join(__dirname, 'db', `${filename}.db`);
-    let db = await database.open(db_name);
-    console.log(`Successful connection to the database ${filename}.db`);
+const db_name = path.join(__dirname, 'db', `motdata.db`);
+const db = new database(db_name, {error: console.log});
 
+const mainCreateDb = async() => {
+  try{
     const sql_create = `CREATE TABLE IF NOT EXISTS motdata (
         registration TEXT,
         make TEXT,
@@ -22,49 +20,74 @@ const main = async() => {
         reason TEXT NULL,
         type TEXT NULL
       );`;
-    await db.run(sql_create);
-    console.log('Successful creation of the \'motdata\' table');
+    const create = db.prepare(sql_create);
+    create.run();
 
-    let page = 1;
+    console.log('Successful creation of the \'motdata\' db and table');
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+
+const mainGetAll = async() => {
+  try {
+    let page = 0;
 
     while (true) {
-      let request = new Date();
-      console.log(`Getting page ${page}`);
-      const response = await axios.get(`https://beta.check-mot.service.gov.uk/trade/vehicles/mot-tests?page=${page}`);
-      console.log('Page complete %dms', new Date() - request);
-
-      let insert = new Date();
-      console.log('Insert into db');
-      const insertSql = `INSERT INTO motdata
-    (registration, make, model, date, result, reason, type)
-    VALUES (?, ?, ?, ?, ?, ?, ?)`;
-
-      for (const vehicle of response.data) {
-        for (const test of vehicle.motTests) {
-          if (test.testResult === 'FAILED') {
-            for (const reason of test.rfrAndComments) {
-              await db.run(insertSql,
-                vehicle.registration,
-                vehicle.make,
-                vehicle.model,
-                test.completedDate,
-                test.testResult,
-                reason.text,
-                reason.type)
-            }
-          } else {
-            await db.run(insertSql,
-              vehicle.registration,
-              vehicle.make,
-              vehicle.model,
-              test.completedDate,
-              test.testResult,
-              null,
-              null)
-          }
+      console.log(`Start get page ${page}`);
+      const response = await retry(async (bail, iteration) => {
+        return await axios.get(`https://beta.check-mot.service.gov.uk/trade/vehicles/mot-tests?page=${page}`);
+      }, {
+        retries: 5,
+        onRetry: (error) => {
+          console.log(`an error occured ${error}`);
         }
-      }
-      console.log('Db Insert complete %dms', new Date() - insert);
+      })
+      console.log(`End get page ${page}`);
+
+      console.log('Start Insert into db');
+      const insertSql = `INSERT INTO motdata
+      (registration, make, model, date, result, reason, type)
+      VALUES (@registration, @make, @model, @date, @result, @reason, @type)`;
+
+      const insert = db.prepare(insertSql);
+      const insertMany = db.transaction((mots) => {
+        for (const mot of mots) {
+          insert.run(mot);
+        }
+      });
+
+      const vehiclesMots = [].concat(...response.data.map((vehicle) => {
+        return [].concat(...vehicle.motTests.map((mot) => {
+          if (mot.testResult === 'FAILED') {
+            return mot.rfrAndComments.map((comment) => {
+              return {
+                registration: vehicle.registration,
+                make: vehicle.make,
+                model: vehicle.model,
+                date: mot.completedDate,
+                result: mot.testResult,
+                reason: comment.text,
+                type: comment.type
+              }
+            });
+          } else
+            return [{
+              registration: vehicle.registration,
+              make: vehicle.make,
+              model: vehicle.model,
+              date: mot.completedDate,
+              result: mot.testResult,
+              reason: null,
+              type: null
+            }]
+        }));
+      }));
+
+      insertMany(vehiclesMots);
+
+      console.log(`End insert into db ${vehiclesMots.length}`);
+
       page++;
     }
   } catch (e) {
@@ -74,13 +97,22 @@ const main = async() => {
 
 program
   .option('--api [api]', 'api key')
+  .option('--create', 'creates the db structure')
+  .option('--getall', 'gets all the data')
+  .option('--getdate [date]', 'get mots on a date')
   .parse(process.argv);
 
-if (!program.api) {
-  console.log('an api key is required')
-  return;
+
+
+if (program.create) {
+  mainCreateDb();
 }
 
-axios.defaults.headers.common['x-api-key'] = program.api
-
-main();
+if (program.getall) {
+  if (!program.api) {
+    console.log('an api key is required')
+    return;
+  }
+  axios.defaults.headers.common['x-api-key'] = program.api
+  mainGetAll();
+}
